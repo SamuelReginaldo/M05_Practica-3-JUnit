0e455a8276a6e7f61c5b367e5b3fcdf52b4f626b
SamuelReginaldo <usr1@gmail.com>
2023-03-23 16:20:07 +0100

    Codificació compleatada dels 3 metodes

--- M05_Practica3/src/main/java/myutils/MyUtils.java
+++ M05_Practica3/src/main/java/myutils/MyUtils.java
@@ -1,11 +1,13 @@
 package myutils;
 
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
 /**
  *
  * @author Samuel Reginaldo
  */
 public class MyUtils {
-
     /**
      *
      * @param cadena Strint cadena que es vol invertir
@@ -13,6 +15,14 @@ public class MyUtils {
      */
     public static String inverteix(String cadena) {
         String resultat = "";
+        
+        if (cadena == null){
+            resultat = null;
+        } else {
+            for (int i = cadena.length() -1; i >= 0; i--) {
+                resultat += cadena.charAt(i);
+            }   
+        }
         return resultat;
     }
 
@@ -28,6 +38,30 @@ public class MyUtils {
      */
     public static int edat(int day, int month, int year) {
         int resultat = 0;
+        Calendar fecha = new GregorianCalendar();
+ 
+        
+        
+        int año = fecha.get(Calendar.YEAR);
+        int mes = fecha.get(Calendar.MONTH);
+        int dia = fecha.get(Calendar.DAY_OF_MONTH);
+        
+        resultat = año - year;
+        
+        if (mes <= month) {
+            resultat -= 1;
+        } else if (mes == month) {
+            if (dia < day){
+               resultat -= 1; 
+            }
+        }
+        
+        if (resultat > 150) {
+            resultat = -1;
+        } else if (resultat <= 0) {
+            resultat = -2;
+        }
+        
         return resultat;
     }
 
@@ -38,12 +72,15 @@ public class MyUtils {
      * -1.
      */
     public static double factorial(double numero) {
-
+        double resultat;
         if (numero == 0) {
-            return 1;
+            resultat = 1;
+        } else if (numero < 0){
+            resultat = -1;
         } else {
-            double resultat = numero * factorial(numero - 1);
-            return resultat;
+            resultat = numero * factorial(numero - 1);
         }
+        
+        return resultat;
     }
 }
